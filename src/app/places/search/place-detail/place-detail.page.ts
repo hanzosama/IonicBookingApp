c584ef0bb515b05e1e4315d72c6c9e3b51581e5a
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2021-11-23 09:00:38 -0500

    ActionSheet Added/

--- src/app/places/search/place-detail/place-detail.page.ts
+++ src/app/places/search/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { ModalController, NavController } from '@ionic/angular';
+import { ActionSheetController, ModalController, NavController } from '@ionic/angular';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
 import { Place } from '../../place.model';
 import { PlacesService } from '../../places-service.service';
@@ -16,7 +16,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(private navController: NavController,
     private route: ActivatedRoute,
     private placesService: PlacesService,
-    private modalCtr: ModalController
+    private modalCtr: ModalController,
+    private actionSheetCtr: ActionSheetController
   ) { }
 
   ngOnInit() {
@@ -30,10 +31,31 @@ export class PlaceDetailPage implements OnInit {
 
   onBookPlace() {
 
-    //this.navController.navigateBack('/places/tabs/search');
-    // this used the stack to know where to go
-    //this.navController.pop();
-    //using modal component
+    this.actionSheetCtr.create({
+      header: 'Choose an action', buttons: [{
+        text: 'Select Date',
+        handler: () => {
+          this.openBookingModal('select');
+        }
+      }, {
+        text: 'Random Date',
+        handler: () => {
+          this.openBookingModal('random');
+        }
+      }, {
+        text: 'Cancel',
+        role: 'cancel'
+      }]
+    }).then(actionSheetEl => {
+      actionSheetEl.present();
+    });
+
+
+  }
+
+  openBookingModal(mode: 'select' | 'random') {
+    console.log(mode);
+
     this.modalCtr.create({ component: CreateBookingComponent, componentProps: { selectedPlace: this.loadedPlace } })
       .then(modalElm => {
         modalElm.present();
