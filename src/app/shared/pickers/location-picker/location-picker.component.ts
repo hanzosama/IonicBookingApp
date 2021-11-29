cc20a968f205013865ad053b3e86ef112704c37b
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2021-11-29 18:06:51 -0500

    Google Map implementation was Added.

---
+++ src/app/shared/pickers/location-picker/location-picker.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { MapModalComponent } from '../map-modal/map-modal.component';
+
+@Component({
+  selector: 'app-location-picker',
+  templateUrl: './location-picker.component.html',
+  styleUrls: ['./location-picker.component.scss'],
+})
+export class LocationPickerComponent implements OnInit {
+  constructor(private modalCtr: ModalController) {}
+
+  ngOnInit() {}
+
+  onPickLocation() {
+    this.modalCtr.create({ component: MapModalComponent }).then(modalEl=>modalEl.present());
+  }
+}
