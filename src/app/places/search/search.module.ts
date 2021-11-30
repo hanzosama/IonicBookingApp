4ba1ae8a295aa7dc70451fe98a9e751d27d3d4d6
Jhoan Mauricio Vivas Rubiano <raiman114@gmail.com>
2021-11-30 17:33:49 -0500

    Adding capacitor modules and replacin ionic scroll component.

--- src/app/places/search/search.module.ts
+++ src/app/places/search/search.module.ts
@@ -8,12 +8,15 @@ import { SearchPageRoutingModule } from './search-routing.module';
 
 import { SearchPage } from './search.page';
 
+import { ScrollingModule } from '@angular/cdk/scrolling';
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SearchPageRoutingModule
+    SearchPageRoutingModule,
+    ScrollingModule,
   ],
   declarations: [SearchPage]
 })
